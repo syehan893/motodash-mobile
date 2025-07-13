5be62d023997e74ba543623e7fbd15250496a9f3
syehan <syehan893@gmail.com>
2025-07-13 11:22:32 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/motodash/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.motodash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\Krowten\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Motodash\motodash"
+export "FLUTTER_APPLICATION_PATH=D:\Motodash\motodash\motodash"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
